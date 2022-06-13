338604a9bd68bd3a1add976ddf85f32b45831c7d
skandog <muumrar@gmail.com>
2022-06-13 10:52:56 +0100

    completed task 3, created comment liast component. Yet to tie it all together and ran out of time. Plan to return to task and complete it

--- src/components/CommentList/index.js
+++ src/components/CommentList/index.js
@@ -1,5 +1,15 @@
-function CommentList() {
-  return null;
+function CommentList({ comments }) {
+  return (
+    <ul className="comment-list">
+      {comments.map((comm) => {
+        <Comment
+          key={comm.id}
+          author={comm.author}
+          content={comm.content}
+        ></Comment>;
+      })}
+    </ul>
+  );
 }
 
 export default CommentList;
