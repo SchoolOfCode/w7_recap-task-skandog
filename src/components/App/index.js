d2d6ef2616e296cc723229bfd5d08f613075a059
skandog <muumrar@gmail.com>
2022-06-13 10:01:51 +0100

    completed task 1, created blogpost component which uses map function and useState in app.js to render list of blog posts

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,39 @@
+/*
+- import react and useState, components
+- define usestate as array of objects
+- return a mapping over array
+
+
+*/
+import React, { useState } from "react";
+import blog from "../../data/blogs";
+import BlogPost from "../BlogPost";
+
 function App() {
-  return null;
+  const [blogList, setBlogList] = useState([blog]);
+
+  console.log(blogList);
+  return (
+    <div className="flex-container">
+      <header>
+        <h1>Skandys blog</h1>
+      </header>
+      <div className="content-container">
+        {blogList.map((post) => {
+          return (
+            <BlogPost
+              title={post.title}
+              author={post.author}
+              datePosted={post.datePosted}
+              content={post.content}
+              imageSrc={post.imageSrc}
+              imageAlt={post.imageAlt}
+            ></BlogPost>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,5 +1,23 @@
-function BlogPost() {
-  return null;
+/* 
+- Return each line of jsx to render blog component
+- need to hand props for each one corresponding to the fields provided
+- no need for state management? Maybe
+
+*/
+
+import React from "react";
+import blog from "../../data/blogs.js";
+
+function BlogPost({ title, author, datePosted, content, imageSrc, imageAlt }) {
+  return (
+    <div className="blog-post">
+      <h1 className="blog-post-title">{title}</h1>
+      <h2 className="blog-post-author">{author}</h2>
+      <h3 className="blog-post-date">{datePosted}</h3>
+      <p className="blog-copy">{content}</p>
+      <img className="blog-img" src={imageSrc} alt={imageAlt} />
+    </div>
+  );
 }
 
 export default BlogPost;
