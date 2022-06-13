9abe2d2c9e5279047361bd9786387a3b73dad60a
skandog <muumrar@gmail.com>
2022-06-13 10:45:39 +0100

    Completed task 2, created comment component, not tested on the page yet as want to create comment list first

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,5 +1,26 @@
-function Comment() {
-  return null;
+/*
+- Need to return jsx for h3 and p tag, with css element next to it
+- need a function to split the name, id first letter of each elt, 
+then feed back into circle
+-need Component to take props of author and content
+
+
+*/
+
+function Comment({ author, content }) {
+  return (
+    <div className="comment">
+      <span className="author-row">
+        <div className="circle">
+          {author.split("").map((nom) => {
+            return nom.charAt(0);
+          })}
+        </div>
+        <div className="author-name">{author}</div>
+      </span>
+      <p className="content">{content}</p>
+    </div>
+  );
 }
 
 export default Comment;
